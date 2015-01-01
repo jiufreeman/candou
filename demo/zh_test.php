65ec92680eb5511e3423952543d031f84ef3aade
Zhang Hao <261726511@qq.com>
2015-01-01 21:53:53 +0800

    1

--- demo/zh_test.php
+++ demo/zh_test.php
@@ -1,6 +1,12 @@
 <?php
 include_once('function.php');
 
-echo geturlc("http://www.qeebu.com");
+echo $html = geturlc("http://movie.douban.com/subject/10807909/?from=showing");
+
+
+
+$data['title'] = preg_match('/<span property="v:itemreviewed">(.*)</span>/',$html);
+echo $data['title'];
+
 
 ?>
