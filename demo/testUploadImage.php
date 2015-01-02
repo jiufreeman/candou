5531d3fd204f4316412965efa4c5f289dcbe4c20
maxwelldu <dcj3sjt@163.com>
2015-01-02 15:48:33 +0800

    add fetch douban photo service

---
+++ demo/testUploadImage.php
@@ -0,0 +1,27 @@
+<?php
+
+//上传图片
+public function uploadImg($content, $filename, $bucket='candou-stage') {
+    //引入又拍云 PHP SDK
+    require_once('upyun.class.php');
+
+    //实例化又拍云对象, bucket, operate user, operate password
+    $upyun = new UpYun($bucket, 'maxwelldu', 'maxwelldu');
+
+    $opts = array(
+        UpYun::CONTENT_MD5 => md5($content)
+    );
+
+    $rsp = $upyun->writeFile("/".$filename, $content, true, $opts);
+    $width = intval($rsp['x-upyun-width']);
+    $height = intval($rsp['x-upyun-height']);
+
+    echo $width;
+    echo $height;
+}
+
+$url = "http://img3.douban.com/view/photo/raw/public/p2218972270.jpg";
+$filename = basename($url);
+$content = file_get_contents($url);
+uploadImg($content, $url);
+
