19f66fa935e90f41a598aae6588b4f73825f1feb
niaiwomattt <xiaocike1@gmail.com>
2015-01-02 18:58:25 +0800

    sphinx config/main.php

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,12 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'components' => [
+        'sphinx' => [
+            'class' => 'yii\sphinx\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;port=9306;',
+            'username' => '',
+            'password' => '',
+        ],
+    ],
 ];
