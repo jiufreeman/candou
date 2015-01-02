d4f62e1f4c24fe1d18f31df77f9d9f9b73e57591
jiufreeman <root@iZ2512odkt5Z.(none)>
2015-01-02 09:49:53 +0800

    change function

--- demo/function.php
+++ demo/function.php
@@ -11,7 +11,9 @@ function geturlc($url){
   curl_setopt($ch, CURLOPT_REFERER, "http://movie.douban.com/");
   curl_setopt($ch, CURLOPT_COOKIE, 'bid='.gen_gid());
   curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
-  return curl_exec($ch);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
 }
 
 function gen_gid(){
