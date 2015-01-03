d6a8f1ee269e567f602b88142c37e3d457eb7f78
maxwelldu <dcj3sjt@163.com>
2015-01-03 09:48:20 +0800

    add fetch douban photo service

---
+++ backend/views/site/sphinx.php
@@ -0,0 +1,27 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+$this->title = 'Sphinx Search';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-sphinx">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please input keywords to search:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'sphinx-form']); ?>
+            <?= $form->field($model, 'username') ?>
+            <div class="form-group">
+                <?= Html::submitButton('Search', ['class' => 'btn btn-primary', 'name' => 'sphinx-button']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
